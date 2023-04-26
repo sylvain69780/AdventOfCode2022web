9698c9ef8c9aecbfefef14115b433d7eeb8b02e4
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-04-26 23:15:25 +0200

    Day10

--- AdventOfCode2022web/Domain/Puzzle/CathodeRayTube.cs
+++ AdventOfCode2022web/Domain/Puzzle/CathodeRayTube.cs
@@ -1,15 +1,53 @@
-﻿namespace AdventOfCode2022web.Domain.Puzzle
+﻿using System.Text;
+
+namespace AdventOfCode2022web.Domain.Puzzle
 {
     public class CathodeRayTube : IPuzzleSolver
     {
         public string Input { get; set; } = String.Empty;
         public string Part1()
         {
-            return "";
+            var input = Input.Split("\n");
+            var x = input.Select(x => x.Split(" "))
+                .SelectMany(x => x[0] == "noop" ? new int[] { 0 } : new int[] { 0, int.Parse(x[1]) });
+            var X = 1;
+            var cycles = 20;
+            var c = 0;
+            var s = 0;
+            foreach (var i in x)
+            {
+                c++;
+                if (c == cycles)
+                {
+                    cycles += 40;
+                    s += X * c;
+                }
+                X += i;
+            }
+            return s.ToString();
         }
         public string Part2()
         {
-            return "";
+            var input = Input.Split("\n");
+            var seq = input.Select(x => x.Split(" "))
+                .SelectMany(x => x[0] == "noop" ? new int[] { 0 } : new int[] { 0, int.Parse(x[1]) });
+            var X = 1;
+            var c = 0;
+            var seq2 = seq.Select(x => (c: ++c, X, X2: X += x)).GetEnumerator();
+            var line = new StringBuilder();
+            var res = new List<string>();
+            foreach (var y in Enumerable.Range(0, 6))
+            {
+                line.Clear();
+                foreach (var x in Enumerable.Range(0, 40))
+                    if (seq2.MoveNext() && seq2.Current.X >= x - 1 && seq2.Current.X <= x + 1)
+                        line.Append('#');
+                    else
+                        line.Append('.');
+                Console.WriteLine(line);
+                res.Add(line.ToString());
+            }
+            return string.Join("\n", res);
         }
     }
 }
--- AdventOfCode2022web/Domain/Puzzle/RopeBridge.cs
+++ AdventOfCode2022web/Domain/Puzzle/RopeBridge.cs
@@ -2,14 +2,82 @@
 {
     public class RopeBridge : IPuzzleSolver
     {
+        struct Pt
+        {
+            public int x;
+            public int y;
+        }
         public string Input { get; set; } = String.Empty;
         public string Part1()
         {
-            return "";
+            var input = Input.Split("\n")
+                .Select(x => x.Split(" "))
+                .SelectMany(x => Enumerable.Range(0, int.Parse(x[1])), (x, y) => x[0]);
+            var directions = new Dictionary<string, Pt>()
+                {
+                    { "R", new Pt{x=1,y=0 } },
+                    { "L", new Pt{x=-1,y=0 } },
+                    { "U", new Pt{x=0,y=1 } },
+                    { "D", new Pt { x =0,y=-1 } },
+                };
+            var visited = new HashSet<(int, int)>();
+            var h = (x: 0, y: 0);
+            var t = (x: 0, y: 0);
+            visited.Add(t);
+            foreach (var d in input)
+            {
+                var dir = directions[d];
+                h.x += dir.x;
+                h.y += dir.y;
+                var (dx, dy) = (h.x - t.x, h.y - t.y);
+                if (Math.Abs(dx) < 2 && Math.Abs(dy) < 2) continue;
+                if (h.x - t.x > 0) t.x++;
+                if (t.x - h.x > 0) t.x--;
+                if (h.y - t.y > 0) t.y++;
+                if (t.y - h.y > 0) t.y--;
+                visited.Add(t);
+            }
+            var result = visited.Count;
+            return result.ToString();
         }
         public string Part2()
         {
-            return "";
+            var input = Input.Split("\n")
+                .Select(x => x.Split(" "))
+                .SelectMany(x => Enumerable.Range(0, int.Parse(x[1])), (x, y) => x[0]);
+            var directions = new Dictionary<string, Pt>()
+            {
+                { "R", new Pt{x=1,y=0 } },
+                { "L", new Pt{x=-1,y=0 } },
+                { "U", new Pt{x=0,y=1 } },
+                { "D", new Pt { x =0,y=-1 } },
+            };
+            var visited = new HashSet<(int, int)>();
+            var h = (x: 0, y: 0);
+            var t = new Pt[9];
+            visited.Add((0, 0));
+            foreach (var d in input)
+            {
+                var dir = directions[d];
+                h.x += dir.x;
+                h.y += dir.y;
+                var prev = new Pt { x = h.x, y = h.y };
+                foreach (var i in Enumerable.Range(0, 9))
+                {
+                    var (dx, dy) = (prev.x - t[i].x, prev.y - t[i].y);
+                    if (Math.Abs(dx) > 1 || Math.Abs(dy) > 1)
+                    {
+                        if (prev.x - t[i].x > 0) t[i].x++;
+                        if (t[i].x - prev.x > 0) t[i].x--;
+                        if (prev.y - t[i].y > 0) t[i].y++;
+                        if (t[i].y - prev.y > 0) t[i].y--;
+                    }
+                    prev = t[i];
+                }
+                visited.Add((t[8].x, t[8].y));
+            }
+            var result = visited.Count;
+            return result.ToString();
         }
     }
 }
