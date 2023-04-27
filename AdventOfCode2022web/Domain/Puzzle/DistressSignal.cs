6248fbe9b9d289ff421a8d4b5b9f44a1e7c2faf5
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-04-27 22:52:20 +0200

    Day13 !

--- AdventOfCode2022web/Domain/Puzzle/DistressSignal.cs
+++ AdventOfCode2022web/Domain/Puzzle/DistressSignal.cs
@@ -2,14 +2,101 @@
 {
     public class DistressSignal : IPuzzleSolver
     {
+        class Day13Element
+        {
+            public static Day13Element ReadInput(string inp)
+            {
+                if (inp[0] == '[')
+                {
+                    if (inp[1] == ']') return new Day13List();
+                    var begin = 1;
+                    var st = new List<string>();
+                    var lvl = 0;
+                    for (var end = 1; end < inp.Length - 2; end++)
+                    {
+                        if (inp[end] == ',' && lvl == 0)
+                        {
+                            st.Add(inp[begin..end]);
+                            begin = end + 1;
+                        }
+                        if (inp[end] == '[') lvl++;
+                        if (inp[end] == ']') lvl--;
+                    }
+                    st.Add(inp[begin..(inp.Length - 1)]);
+                    return new Day13List
+                    {
+                        Lst = st.Select(x => ReadInput(x)).ToList()
+                    };
+                }
+                else
+                {
+                    return new Day13Leaf() { Val = int.Parse(inp) };
+                }
+            }
+            public static int IsOrdered(Day13Element a, Day13Element b)
+            {
+                if (a is Day13Leaf av && b is Day13Leaf bv)
+                    return av.Val < bv.Val ? 1 : av.Val == bv.Val ? 0 : -1;
+                if (a is Day13Leaf)
+                    a = new Day13List { Lst = new List<Day13Element> { a } };
+                if (b is Day13Leaf)
+                    b = new Day13List { Lst = new List<Day13Element> { b } };
+                var al = (Day13List)a;
+                var bl = (Day13List)b;
+                for (var i = 0; i < Math.Min(al.Lst.Count, bl.Lst.Count); i++)
+                {
+                    var va = al.Lst[i];
+                    var vb = bl.Lst[i];
+                    var res = IsOrdered(va, vb);
+                    if (res != 0) return res;
+                }
+                return al.Lst.Count < bl.Lst.Count ? 1 : al.Lst.Count == bl.Lst.Count ? 0 : -1;
+            }
+        }
+         class Day13Leaf : Day13Element
+        {
+            public int Val;
+        }
+         class Day13List : Day13Element
+        {
+            public List<Day13Element> Lst = new();
+        }
+
+        class MyComparer : IComparer<Day13Element>
+        {
+
+            public int Compare(Day13Element A, Day13Element B)
+            {
+                return Day13Element.IsOrdered(A, B);
+            }
+
+        }
+
         public string Input { get; set; } = String.Empty;
         public string Part1()
         {
-            return "";
+            var input = Input.Split("\n").ToList().GetEnumerator();
+            var idx = 0;
+            var score = 0;
+            while (input.MoveNext())
+            {
+                idx++;
+                var p1 = Day13Element.ReadInput(input.Current);
+                input.MoveNext();
+                var p2 = Day13Element.ReadInput(input.Current);
+                input.MoveNext();
+                var r = Day13Element.IsOrdered(p1, p2);
+                Console.WriteLine(r);
+                if (r == 1) score += idx;
+            }
+            return score.ToString();
         }
         public string Part2()
         {
-            return "";
+            var input = Input.Split("\n").Where(x => x != "").Append("[[2]]").Append("[[6]]").Select(x => (a: x, b: Day13Element.ReadInput(x)))
+                .OrderByDescending(x => x.b, new MyComparer()).Select(x => x.a).ToList();
+
+            return ((1 + input.IndexOf("[[2]]")) * (1 + input.IndexOf("[[6]]"))).ToString();
         }
     }
 }
--- AdventOfCode2022web/Domain/Puzzle/HillClimbingAlgorithm.cs
+++ AdventOfCode2022web/Domain/Puzzle/HillClimbingAlgorithm.cs
@@ -5,11 +5,96 @@
         public string Input { get; set; } = String.Empty;
         public string Part1()
         {
-            return "";
+            var input = Input.Split("\n");
+            var gridWidth = input[0].Length;
+            var gridHeight = input.Length;
+            var directions = new List<(int, int)> { (1, 0), (-1, 0), (0, 1), (0, -1) };
+            var start = (x: 0, y: 0);
+            foreach (var l in input)
+            {
+                var s = l.IndexOf('S');
+                if (s != -1)
+                {
+                    start.x = s;
+                    break;
+                }
+                start.y++;
+            }
+            var explored = new HashSet<(int, int)>() { start };
+            var queue = new Queue<(int, int)>();
+            queue.Enqueue(start);
+            var getH = (char c) => { if (c == 'S') c = 'a'; if (c == 'E') c = 'z'; return (int)c - (int)'a'; };
+            var score = 0;
+            while (queue.Count > 0)
+            {
+                score++;
+                var newQueue = new Queue<(int, int)>();
+                while (queue.Count > 0)
+                {
+                    var (x, y) = queue.Dequeue();
+                    var c = input[y][x];
+                    var h = getH(c);
+                    foreach (var (dx, dy) in directions)
+                    {
+                        var ne = (x: x + dx, y: y + dy);
+                        if (ne.x < 0 || ne.x >= gridWidth || ne.y < 0 || ne.y >= gridHeight || explored.Contains(ne)) continue;
+                        var nc = input[ne.y][ne.x];
+                        var nh = getH(nc);
+                        if (nh - h < 2)
+                        {
+                            newQueue.Enqueue(ne);
+                            explored.Add(ne);
+                            if (nc == 'E') { newQueue.Clear(); queue.Clear(); break; }
+                        }
+                    }
+                }
+                queue = newQueue;
+            }
+            Console.WriteLine(explored.Count);
+            return score.ToString();
         }
         public string Part2()
         {
-            return "";
+            var input = Input.Split("\n");
+            var gridWidth = input[0].Length;
+            var gridHeight = input.Length;
+            var directions = new List<(int, int)> { (1, 0), (-1, 0), (0, 1), (0, -1) };
+            var start = (x: 0, y: 0);
+            var queue = new Queue<(int, int)>();
+            for (var y = 0; y < gridHeight; y++)
+                for (var x = 0; x < gridWidth; x++)
+                    if (input[y][x] == 'a') queue.Enqueue((x, y));
+            var explored = new HashSet<(int, int)>() { start };
+            queue.Enqueue(start);
+            var getH = (char c) => { if (c == 'S') c = 'a'; if (c == 'E') c = 'z'; return (int)c - (int)'a'; };
+            var score = 0;
+            while (queue.Count > 0)
+            {
+                score++;
+                var newQueue = new Queue<(int, int)>();
+                while (queue.Count > 0)
+                {
+                    var (x, y) = queue.Dequeue();
+                    var c = input[y][x];
+                    var h = getH(c);
+                    foreach (var (dx, dy) in directions)
+                    {
+                        var ne = (x: x + dx, y: y + dy);
+                        if (ne.x < 0 || ne.x >= gridWidth || ne.y < 0 || ne.y >= gridHeight || explored.Contains(ne)) continue;
+                        var nc = input[ne.y][ne.x];
+                        var nh = getH(nc);
+                        if (nh - h < 2)
+                        {
+                            newQueue.Enqueue(ne);
+                            explored.Add(ne);
+                            if (nc == 'E') { newQueue.Clear(); queue.Clear(); break; }
+                        }
+                    }
+                }
+                queue = newQueue;
+            }
+            Console.WriteLine(explored.Count);
+            return score.ToString();
         }
     }
 }
