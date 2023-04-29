26c194614f371829f5e514072c5e6d85dc626b93
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-04-29 23:28:56 +0200

    clean part3

--- AdventOfCode2022web/Domain/Puzzle/RockPaperScissors.cs
+++ AdventOfCode2022web/Domain/Puzzle/RockPaperScissors.cs
@@ -6,31 +6,37 @@ namespace AdventOfCode2022web.Domain.Puzzle
     public class RockPaperScissors : PuzzleSolver
     {
         private static string[] ToLines(string s) => s.Split("\n");
+        private static string Format(int v) => v.ToString();
+
         private enum Moves { Rock = 0, Paper = 1, Scissors = 2 };
         private enum GameResults { Lose = 0, Draw = 1, Win = 2 };
-        // Rules
+
+        /// <summary>
+        /// Rules of the Game
+        /// </summary>
         private static readonly (Moves OpponentPlayed, Moves YouPlayed)[] RoundIsLost = {
                 (Moves.Rock, Moves.Scissors),
                 (Moves.Scissors, Moves.Paper),
                 (Moves.Paper,Moves.Rock)
             };
-        // Decodes Input PART 1
-        // A = X = Rock 
-        // B = Y = Paper 
-        // C = Z = Scissors 
-        private static (Moves OpponentPlayed, Moves YouPlayed) Decode1(string s) 
+
+        /// <summary>Decodes Input PART 1 A = X = Rock B = Y = Paper C = Z = Scissors</summary>
+        /// <param name="s"></param>
+        /// <returns>OpponentPlayed YouPlayed tuple</returns>
+        private static (Moves OpponentPlayed, Moves YouPlayed) DecodeMovesPart1(string s) 
             => ((Moves)(s[0] - 'A'),(Moves)(s[2] - 'X'));
-        // Decodes Input PART 2
-        // X = Lose
-        // Y = Draw
-        // Z = Win
-        private static (Moves OpponentPlayed, GameResults ExpectedResult) Decode2(string s)
+
+        /// <summary>Decodes Input PART 2 X = Lose Y = Draw Z = Win</summary>
+        /// <param name="s"></param>
+        /// <returns>OpponentPlayed ExpectedResult tuple</returns>
+        private static (Moves OpponentPlayed, GameResults ExpectedResult) DecodeMovesPart2(string s)
             => ((Moves)(s[0] - 'A'), (GameResults)(s[2] - 'X'));
-        private static string Format(int v) => v.ToString();
+
+
         protected override string Part1(string puzzleInput)
         {
             var score = 0;
-            foreach (var round in ToLines(puzzleInput).Select(x => Decode1(x)))
+            foreach (var round in ToLines(puzzleInput).Select(x => DecodeMovesPart1(x)))
             {
                 score += (int)round.YouPlayed + 1;
                 if (!RoundIsLost.Contains(round))
@@ -38,10 +44,11 @@ namespace AdventOfCode2022web.Domain.Puzzle
             }
             return Format(score);
         }
+
         protected override string Part2(string puzzleInput)
         {
             var score = 0;
-            foreach (var round in ToLines(puzzleInput).Select(x => Decode2(x)))
+            foreach (var round in ToLines(puzzleInput).Select(x => DecodeMovesPart2(x)))
             {
                 var youPlay = round.OpponentPlayed;
                 if (round.ExpectedResult == GameResults.Lose)
--- AdventOfCode2022web/Domain/Puzzle/RucksackReorganization.cs
+++ AdventOfCode2022web/Domain/Puzzle/RucksackReorganization.cs
@@ -2,31 +2,35 @@
 {
     public class RucksackReorganization : PuzzleSolver
     {
-        protected override string Part1(string inp)
+        private static string[] ToLines(string s) => s.Split("\n");
+        private static string Format(int v) => v.ToString();
+
+        protected override string Part1(string puzzleInput)
         {
             var score = 0;
-            foreach (var rucksack in inp.Split("\n"))
+            foreach (var rucksack in ToLines(puzzleInput))
             {
-                var l = rucksack.Length;
-                var (a, b) = (rucksack.Substring(0, l / 2), rucksack.Substring(l / 2, l / 2));
-                var r = (int)a.First(x => b.Contains(x));
-                var p = r >= (int)'a' ? r - (int)'a' + 1 : r - (int)'A' + 27;
-                score += p;
+                var compartmentSize = rucksack.Length /2;
+                var (compartmentA, compartmentB) = (rucksack.Substring(0, compartmentSize), rucksack.Substring(compartmentSize, compartmentSize));
+                var shared = (int)compartmentA.First(x => compartmentB.Contains(x));
+                var priority = shared >= (int)'a' ? shared - (int)'a' + 1 : shared - (int)'A' + 27;
+                score += priority;
             }
-            return score.ToString();
+            return Format(score);
         }
-        protected override string Part2(string inp)
+
+        protected override string Part2(string puzzleInput)
         {
             var score = 0;
-            var rucksacks = inp.Split("\n");
+            var rucksacks = ToLines(puzzleInput);
             for (var i = 0; i < rucksacks.Length / 3; i++)
             {
                 var (a, b, c) = (rucksacks[i * 3], rucksacks[i * 3 + 1], rucksacks[i * 3 + 2]);
-                var r = (int)a.First(x => b.Contains(x) && c.Contains(x));
-                var p = r >= (int)'a' ? r - (int)'a' + 1 : r - (int)'A' + 27;
-                score += p;
+                var badge = (int)a.First(x => b.Contains(x) && c.Contains(x));
+                var priority = badge >= (int)'a' ? badge - (int)'a' + 1 : badge - (int)'A' + 27;
+                score += priority;
             }
-            return score.ToString();
+            return Format(score);
         }
     }
 }
