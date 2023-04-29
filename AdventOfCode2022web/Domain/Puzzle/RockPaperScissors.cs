7aa789e89c6608e3a03d5b734ca54b349d412b54
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-04-29 22:43:28 +0200

    Clean day 2

--- AdventOfCode2022web/Domain/Puzzle/RockPaperScissors.cs
+++ AdventOfCode2022web/Domain/Puzzle/RockPaperScissors.cs
@@ -1,34 +1,57 @@
-﻿namespace AdventOfCode2022web.Domain.Puzzle
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022web.Domain.Puzzle
 {
     public class RockPaperScissors : PuzzleSolver
     {
-        protected override string Part1(string input)
+        private static string[] ToLines(string s) => s.Split("\n");
+        private enum Moves { Rock = 0, Paper = 1, Scissors = 2 };
+        private enum GameResults { Lose = 0, Draw = 1, Win = 2 };
+        // Rules
+        private static readonly (Moves OpponentPlayed, Moves YouPlayed)[] RoundIsLost = {
+                (Moves.Rock, Moves.Scissors),
+                (Moves.Scissors, Moves.Paper),
+                (Moves.Paper,Moves.Rock)
+            };
+        // Decodes Input PART 1
+        // A = X = Rock 
+        // B = Y = Paper 
+        // C = Z = Scissors 
+        private static (Moves OpponentPlayed, Moves YouPlayed) Decode1(string s) 
+            => ((Moves)(s[0] - 'A'),(Moves)(s[2] - 'X'));
+        // Decodes Input PART 2
+        // X = Lose
+        // Y = Draw
+        // Z = Win
+        private static (Moves OpponentPlayed, GameResults ExpectedResult) Decode2(string s)
+            => ((Moves)(s[0] - 'A'), (GameResults)(s[2] - 'X'));
+        private static string Format(int v) => v.ToString();
+        protected override string Part1(string puzzleInput)
         {
-            var defeats = new List<(int, int)> { (1, 3), (3, 2), (2, 1) };
             var score = 0;
-            foreach (var (opponent, you) in input.Split("\n").Select(x => x.Split(" ")).Select(x => ("ABC".IndexOf(x[0][0]) + 1, "XYZ".IndexOf(x[1][0]) + 1)))
+            foreach (var round in ToLines(puzzleInput).Select(x => Decode1(x)))
             {
-                score += you;
-                if (!defeats.Contains((opponent, you)))
-                    score += you == opponent ? 3 : 6;
+                score += (int)round.YouPlayed + 1;
+                if (!RoundIsLost.Contains(round))
+                    score += round.YouPlayed == round.OpponentPlayed ? 3 : 6;
             }
-            return score.ToString();
+            return Format(score);
         }
-        protected override string Part2(string input)
+        protected override string Part2(string puzzleInput)
         {
             var score = 0;
-            var defeats = new List<(int, int)> { (1, 3), (3, 2), (2, 1) };
-            foreach (var (opponent, ending) in input.Split("\n").Select(x => x.Split(" ")).Select(x => ("ABC".IndexOf(x[0][0]) + 1, "XYZ".IndexOf(x[1][0]) + 1)))
+            foreach (var round in ToLines(puzzleInput).Select(x => Decode2(x)))
             {
-                var you = opponent;
-                if (ending == 1)
-                    you = defeats.Find(x => x.Item1 == opponent).Item2;
-                else if (ending == 3)
-                    you = defeats.Find(x => x.Item2 == opponent).Item1;
-                score += you;
-                score += (ending - 1) * 3;
+                var youPlay = round.OpponentPlayed;
+                if (round.ExpectedResult == GameResults.Lose)
+                    youPlay = RoundIsLost.First(x => x.OpponentPlayed == round.OpponentPlayed).YouPlayed;
+                if (round.ExpectedResult == GameResults.Win)
+                    youPlay = RoundIsLost.First(x => x.YouPlayed == round.OpponentPlayed).OpponentPlayed;
+                score += (int)youPlay + 1;
+                score += (int)round.ExpectedResult * 3;
             }
-            return score.ToString();
+            return Format(score);
         }
     }
 }
