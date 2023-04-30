331f4c7e3d3a36c715969dae3cfbdffa0df296a7
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-04-30 12:34:47 +0200

    CampCleanup Clean

--- AdventOfCode2022web/Domain/Puzzle/CampCleanup.cs
+++ AdventOfCode2022web/Domain/Puzzle/CampCleanup.cs
@@ -1,28 +1,62 @@
-﻿namespace AdventOfCode2022web.Domain.Puzzle
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022web.Domain.Puzzle
 {
     public class CampCleanup : PuzzleSolver
     {
-        protected override string Part1(string inp)
+        private static string[] ToLines(string s) => s.Split("\n");
+        private static string Format(int v) => v.ToString();
+
+        private readonly struct Interval
         {
-            var r = (string x) => { var r = x.Split("-"); return (start: int.Parse(r[0]), end: int.Parse(r[1])); };
-            var score = 0;
-            foreach (var (a, b) in inp.Split("\n").Select(x => x.Split(",")).Select(x => (r(x[0]), r(x[1]))))
+            public int Start { get; }
+            public int End { get; }
+
+            public Interval(int start, int end)
             {
-                if ((b.start >= a.start && b.end <= a.end) || (a.start >= b.start && a.end <= b.end))
-                    score++;
+                if (end < start)
+                    throw new ArgumentException("End must be greater than or equal to start.");
+                Start = start;
+                End = end;
             }
-            return score.ToString();
+
+            public bool Contains(Interval a) => a.Start >= Start && a.End <= End;
+
+            public bool Overlaps(Interval a) => a.Start <= End && a.End >= Start;
         }
-        protected override string Part2(string inp)
+
+        private static Interval ToInterval(string a)
         {
-            var r = (string x) => { var r = x.Split("-"); return (start: int.Parse(r[0]), end: int.Parse(r[1])); };
-            var score = 0;
-            foreach (var (a, b) in inp.Split("\n").Select(x => x.Split(",")).Select(x => (r(x[0]), r(x[1]))))
+            var b = a.Split("-");
+            return new Interval(int.Parse(b[0]), int.Parse(b[1]));
+        }
+
+        private static IEnumerable<(Interval Interval1, Interval Interval2)> ListOfSectionAssignmentPairs(IEnumerable<string> records)
+        {
+            foreach (var a in records)
             {
-                if ((a.start <= b.start && b.start <= a.end) || (b.start <= a.start && a.start <= b.end))
-                    score++;
+                var b = a.Split(",");
+                (string part1, string part2) = (b[0], b[1]);
+                yield return (ToInterval(part1), ToInterval(part2));
             }
-            return score.ToString();
+        }
+
+        protected override string Part1(string puzzleInput)
+        {
+            var score = 0;
+            foreach (var (a, b) in ListOfSectionAssignmentPairs(ToLines(puzzleInput)))
+                if (a.Contains(b) || b.Contains(a)) 
+                    score++;
+            return Format(score);
+        }
+
+        protected override string Part2(string puzzleInput)
+        {
+            var score = 0;
+            foreach (var (a, b) in ListOfSectionAssignmentPairs(ToLines(puzzleInput)))
+                if (a.Overlaps(b))
+                    score++;
+            return Format(score);
         }
     }
 }
--- AdventOfCode2022web/Domain/Puzzle/RucksackReorganization.cs
+++ AdventOfCode2022web/Domain/Puzzle/RucksackReorganization.cs
@@ -1,20 +1,28 @@
-﻿namespace AdventOfCode2022web.Domain.Puzzle
+﻿using System;
+
+namespace AdventOfCode2022web.Domain.Puzzle
 {
     public class RucksackReorganization : PuzzleSolver
     {
         private static string[] ToLines(string s) => s.Split("\n");
         private static string Format(int v) => v.ToString();
 
+        /// <summary>
+        /// Every item type can be converted to a priority
+        /// Lowercase item types a through z have priorities 1 through 26.
+        /// Uppercase item types A through Z have priorities 27 through 52.
+        /// </summary>
+        private static int Priority(char item) => item >= 'a' ? (item - 'a') + 1 : (item - 'A') + 27;
+
         protected override string Part1(string puzzleInput)
         {
             var score = 0;
             foreach (var rucksack in ToLines(puzzleInput))
             {
-                var compartmentSize = rucksack.Length /2;
+                var compartmentSize = rucksack.Length / 2;
                 var (compartmentA, compartmentB) = (rucksack.Substring(0, compartmentSize), rucksack.Substring(compartmentSize, compartmentSize));
-                var shared = (int)compartmentA.First(x => compartmentB.Contains(x));
-                var priority = shared >= (int)'a' ? shared - (int)'a' + 1 : shared - (int)'A' + 27;
-                score += priority;
+                var sharedItem = compartmentA.First(x => compartmentB.Contains(x));
+                score += Priority(sharedItem);
             }
             return Format(score);
         }
@@ -25,10 +33,9 @@
             var rucksacks = ToLines(puzzleInput);
             for (var i = 0; i < rucksacks.Length / 3; i++)
             {
-                var (a, b, c) = (rucksacks[i * 3], rucksacks[i * 3 + 1], rucksacks[i * 3 + 2]);
-                var badge = (int)a.First(x => b.Contains(x) && c.Contains(x));
-                var priority = badge >= (int)'a' ? badge - (int)'a' + 1 : badge - (int)'A' + 27;
-                score += priority;
+                var (firstGroup, secondGroup, thirdGroup) = (rucksacks[i * 3], rucksacks[i * 3 + 1], rucksacks[i * 3 + 2]);
+                var badge = firstGroup.First(x => secondGroup.Contains(x) && thirdGroup.Contains(x));
+                score += Priority(badge);
             }
             return Format(score);
         }
