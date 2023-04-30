bb1cf3ee22380adcdb411f6e84e4bb165b18a8b2
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-04-30 17:07:04 +0200

    day8

--- AdventOfCode2022web/Domain/Puzzle/TreetopTreeHouse.cs
+++ AdventOfCode2022web/Domain/Puzzle/TreetopTreeHouse.cs
@@ -2,98 +2,106 @@
 {
     public class TreetopTreeHouse : PuzzleSolver
     {
+        private static string[] ToLines(string s) => s.Split("\n");
+        private static string Format(int v) => v.ToString();
 
-        protected override string Part1(string inp)
+
+        private class HeightMap
         {
-            var input = inp.Split("\n");
-            var gridWidth = input[0].Length;
-            var gridHeight = input.Length;
-            var explored = new HashSet<(int, int)>();
-            var getGrid = (int x, int y) => (int)input[y][x] - (int)'0';
-            foreach (var y in Enumerable.Range(0, gridHeight))
+            public string[] Map { get; set; } = new string[0];
+
+            public HeightMap(string[] map)
+            {
+                Map = map;
+            }
+            public int Width => Map[0].Length;
+            public int Height => Map.Length;
+            public int TreeHeight(int x, int y) => (int)Map[y][x] - (int)'0';
+            public bool BorderReached(int x, int y) => x < 0 || x >= Width || y < 0 || y >= Height;
+        }
+
+        protected override string Part1(string puzzleInput)
+        {
+            var map = new HeightMap(ToLines(puzzleInput));
+            var visibleTrees = new HashSet<(int x, int y)>();
+            foreach (var y in Enumerable.Range(0, map.Height))
             {
                 // left to right
                 var hmax = -1;
-                foreach (var x in Enumerable.Range(0, gridWidth))
+                foreach (var x in Enumerable.Range(0, map.Width))
                 {
-                    var h = getGrid(x, y);
+                    var h = map.TreeHeight(x, y);
                     if (h > hmax)
                     {
                         hmax = h;
-                        if (!explored.Contains((x, y))) explored.Add((x, y));
+                        if (!visibleTrees.Contains((x, y))) visibleTrees.Add((x, y));
                     }
                 }
                 // right to left
                 hmax = -1;
-                foreach (var x in Enumerable.Range(0, gridWidth).Reverse())
+                foreach (var x in Enumerable.Range(0, map.Width).Reverse())
                 {
-                    var h = getGrid(x, y);
+                    var h = map.TreeHeight(x, y);
                     if (h > hmax)
                     {
                         hmax = h;
-                        if (!explored.Contains((x, y))) explored.Add((x, y));
+                        if (!visibleTrees.Contains((x, y))) visibleTrees.Add((x, y));
                     }
                 }
             }
-            foreach (var x in Enumerable.Range(0, gridWidth))
+            foreach (var x in Enumerable.Range(0, map.Width))
             {
                 // top to down
                 var hmax = -1;
-                foreach (var y in Enumerable.Range(0, gridHeight))
+                foreach (var y in Enumerable.Range(0, map.Height))
                 {
-                    var h = getGrid(x, y);
+                    var h = map.TreeHeight(x, y);
                     if (h > hmax)
                     {
                         hmax = h;
-                        if (!explored.Contains((x, y))) explored.Add((x, y));
+                        if (!visibleTrees.Contains((x, y))) visibleTrees.Add((x, y));
                     }
                 }
                 // down to top
                 hmax = -1;
-                foreach (var y in Enumerable.Range(0, gridHeight).Reverse())
+                foreach (var y in Enumerable.Range(0, map.Height).Reverse())
                 {
-                    var h = getGrid(x, y);
+                    var h = map.TreeHeight(x, y);
                     if (h > hmax)
                     {
                         hmax = h;
-                        if (!explored.Contains((x, y))) explored.Add((x, y));
+                        if (!visibleTrees.Contains((x, y))) visibleTrees.Add((x, y));
                     }
                 }
             }
-            Console.WriteLine(explored.Count);
-            return explored.Count.ToString();
+            return Format(visibleTrees.Count);
         }
-        protected override string Part2(string inp)
+
+        private static readonly (int, int)[] Directions = new (int x, int y)[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
+
+        protected override string Part2(string puzzleInput)
         {
-            var input = inp.Split("\n");
-            var gridWidth = input[0].Length;
-            var gridHeight = input.Length;
-            var explored = new HashSet<(int, int)>();
-            var getGrid = (int x, int y) => (int)input[y][x] - (int)'0';
-            var directions = new List<(int, int)> { (1, 0), (-1, 0), (0, 1), (0, -1) };
+            var map = new HeightMap(ToLines(puzzleInput));
             var scoreMax = 0;
-            var borderReached = (int x, int y) => x < 0 || x >= gridWidth || y < 0 || y >= gridHeight;
-            foreach (var y in Enumerable.Range(0, gridHeight))
-                foreach (var x in Enumerable.Range(0, gridWidth))
+            foreach (var yTree in Enumerable.Range(0, map.Height))
+                foreach (var xTree in Enumerable.Range(0, map.Width))
                 {
-                    var h = getGrid(x, y);
+                    var treeHeight = map.TreeHeight( xTree, yTree);
                     var score = 1;
-                    foreach (var (dx, dy) in directions)
+                    foreach (var (dx, dy) in Directions)
                     {
-                        var i = 1;
-                        var (nx, ny) = (x, y);
-                        do
+                        var distance = 1;
+                        var (x, y) = (xTree + distance * dx, yTree + distance * dy);
+                        while ( !map.BorderReached(x, y) && map.TreeHeight(x, y) < treeHeight ) 
                         {
-                            (nx, ny) = (x + i * dx, y + i * dy);
-                            if (borderReached(nx, ny)) break;
-                            i++;
-                        } while (getGrid(nx, ny) < h);
-                        score *= (i - 1);
+                            distance++;
+                            (x, y) = (xTree + distance * dx, yTree + distance * dy);
+                        } 
+                        score *= distance - (map.BorderReached(x, y) ? 1:0);
                     }
-                    scoreMax = score > scoreMax ? score : scoreMax;
+                    scoreMax = Math.Max(score,scoreMax);
                 }
-            Console.WriteLine(scoreMax);
-            return scoreMax.ToString();
+            return Format(scoreMax);
         }
     }
 }
