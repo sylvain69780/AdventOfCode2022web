89382f203399707ebaf802d75f0dc941fc47beca
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-07-06 23:44:41 +0200

    24 in progress

--- AdventOfCode2022/Puzzles/BlizzardBasin.cs
+++ AdventOfCode2022/Puzzles/BlizzardBasin.cs
@@ -1,14 +1,19 @@
 ﻿namespace AdventOfCode2022web.Puzzles
 {
     [Puzzle(24, "Blizzard Basin")]
-    public class BlizzardBasin : IPuzzleSolver
+    public class BlizzardBasin : IPuzzleSolverV3
     {
-        public string SolveFirstPart(string inp)
+        public string Visualize()
         {
-            var input = inp.Split("\n");
-            var start = (x: input[0].IndexOf('.'), y: 0);
-            var arrival = (x: input[^1].IndexOf('.'), y: input.Length - 1);
-            var walls = input
+            return string.Empty;
+        }
+
+        public void Setup(string puzzleInput)
+        {
+            var input = puzzleInput.Split("\n");
+            Start = (x: input[0].IndexOf('.'), y: 0);
+            Arrival = (x: input[^1].IndexOf('.'), y: input.Length - 1);
+            Walls = input
                 .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
                 .Where(y => y.c == '#'))
                 .Select(e => (x: e.col, y: e.row))
@@ -19,172 +24,159 @@
                 .Where(y => blizzardsTypes.Contains(y.c)))
                 .Select(e => (x: e.col, y: e.row, e.c))
                 .ToList();
-            var blizzardsRight = blizzards.Where(e => e.c == '>').ToList();
-            var blizzardsLeft = blizzards.Where(e => e.c == '<').ToList();
-            var blizzardsUp = blizzards.Where(e => e.c == '^').ToList();
-            var blizzardsDown = blizzards.Where(e => e.c == 'v').ToList();
-            var width = input[0].Length - 2;
-            var height = input.Length - 2;
+            BlizzardsRight = blizzards.Where(e => e.c == '>').ToList();
+            BlizzardsLeft = blizzards.Where(e => e.c == '<').ToList();
+            BlizzardsUp = blizzards.Where(e => e.c == '^').ToList();
+            BlizzardsDown = blizzards.Where(e => e.c == 'v').ToList();
+            Width = input[0].Length - 2;
+            Height = input.Length - 2;
+        }
 
+        private (int x, int y) Start;
+        private (int x, int y) Arrival;
+        private List<(int x, int y, char c)> BlizzardsRight;
+        private List<(int x, int y, char c)> BlizzardsLeft;
+        private List<(int x, int y, char c)> BlizzardsUp;
+        private List<(int x, int y, char c)> BlizzardsDown;
+        private int Width;
+        private int Height;
+        private HashSet<(int x, int y)> Walls;
+
+        private static readonly List<(int dx, int dy)> Moves = new List<(int dx, int dy)>()
+        {
+            (0,0),
+            (1,0),
+            (-1,0),
+            (0,1),
+            (0,-1)
+        };
+
+
+        public IEnumerable<string> SolveFirstPart()
+        {
             var minute = 0;
             var search = new Queue<(int x, int y)>();
-            var moves = new List<(int dx, int dy)>()
-    {
-        (0,0),
-        (1,0),
-        (-1,0),
-        (0,1),
-        (0,-1)
-    };
             var mod = (int x, int m) => (x % m + m) % m;
-            search.Enqueue(start);
+            search.Enqueue(Start);
             do
             {
                 minute++;
                 var newSearch = new Queue<(int x, int y)>();
                 // compute blizzards positions
-                var blizzardsPos = blizzardsRight.Select(e => ((e.x - 1 + minute) % width + 1, e.y)).ToHashSet();
-                blizzardsPos.UnionWith(blizzardsLeft.Select(e => (mod(e.x - 1 - minute, width) + 1, e.y)));
-                blizzardsPos.UnionWith(blizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, height) + 1)));
-                blizzardsPos.UnionWith(blizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % height + 1)));
+                var blizzardsPos = BlizzardsRight.Select(e => ((e.x - 1 + minute) % Width + 1, e.y)).ToHashSet();
+                blizzardsPos.UnionWith(BlizzardsLeft.Select(e => (mod(e.x - 1 - minute, Width) + 1, e.y)));
+                blizzardsPos.UnionWith(BlizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, Height) + 1)));
+                blizzardsPos.UnionWith(BlizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % Height + 1)));
                 while (search.TryDequeue(out var expedition))
                 {
-                    foreach (var (dx, dy) in moves)
+                    foreach (var (dx, dy) in Moves)
                     {
                         var pos = (x: expedition.x + dx, y: expedition.y + dy);
-                        if (pos == arrival)
+                        if (pos == Arrival)
                         {
-                            return $"FOUND {minute}";
+                            yield return $"FOUND {minute}";
+                            yield break;
                         }
-                        if (pos.y >= 0 && !blizzardsPos.Contains(pos) && !walls.Contains(pos) && !newSearch.Contains(pos))
+                        if (pos.y >= 0 && !blizzardsPos.Contains(pos) && !Walls.Contains(pos) && !newSearch.Contains(pos))
                             newSearch.Enqueue(pos);
                     }
                 }
                 search = newSearch;
             } while (search.Count > 0);
-            return $"NOT FOUND {minute}";
+            yield return $"NOT FOUND {minute}";
         }
-        public string SolveSecondPart(string inp)
+        public IEnumerable<string> SolveSecondPart()
         {
-            var input = inp.Split("\n");
-            var start = (x: input[0].IndexOf('.'), y: 0);
-            var arrival = (x: input[^1].IndexOf('.'), y: input.Length - 1);
-            var walls = input
-                .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
-                .Where(y => y.c == '#'))
-                .Select(e => (x: e.col, y: e.row))
-                .ToHashSet();
-            var blizzardsTypes = new char[] { '>', '<', '^', 'v' };
-            var blizzards = input
-                .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
-                .Where(y => blizzardsTypes.Contains(y.c)))
-                .Select(e => (x: e.col, y: e.row, e.c))
-                .ToList();
-            var blizzardsRight = blizzards.Where(e => e.c == '>').ToList();
-            var blizzardsLeft = blizzards.Where(e => e.c == '<').ToList();
-            var blizzardsUp = blizzards.Where(e => e.c == '^').ToList();
-            var blizzardsDown = blizzards.Where(e => e.c == 'v').ToList();
-            var width = input[0].Length - 2;
-            var height = input.Length - 2;
-
             var minute = 0;
             var search = new Queue<(int x, int y)>();
-            var moves = new List<(int dx, int dy)>()
-    {
-        (0,0),
-        (1,0),
-        (-1,0),
-        (0,1),
-        (0,-1)
-    };
             var mod = (int x, int m) => (x % m + m) % m;
-            search.Enqueue(start);
+            search.Enqueue(Start);
             do
             {
                 minute++;
                 var newSearch = new Queue<(int x, int y)>();
                 // compute blizzards positions
-                var blizzardsPos = blizzardsRight.Select(e => ((e.x - 1 + minute) % width + 1, e.y)).ToHashSet();
-                blizzardsPos.UnionWith(blizzardsLeft.Select(e => (mod(e.x - 1 - minute, width) + 1, e.y)));
-                blizzardsPos.UnionWith(blizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, height) + 1)));
-                blizzardsPos.UnionWith(blizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % height + 1)));
+                var blizzardsPos = BlizzardsRight.Select(e => ((e.x - 1 + minute) % Width + 1, e.y)).ToHashSet();
+                blizzardsPos.UnionWith(BlizzardsLeft.Select(e => (mod(e.x - 1 - minute, Width) + 1, e.y)));
+                blizzardsPos.UnionWith(BlizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, Height) + 1)));
+                blizzardsPos.UnionWith(BlizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % Height + 1)));
                 while (search.TryDequeue(out var expedition))
                 {
-                    foreach (var (dx, dy) in moves)
+                    foreach (var (dx, dy) in Moves)
                     {
                         var pos = (x: expedition.x + dx, y: expedition.y + dy);
-                        if (pos == arrival)
+                        if (pos == Arrival)
                         {
                             Console.WriteLine($"FOUND {minute}");
                             newSearch.Clear();
                             search.Clear();
                             break;
                         }
-                        if (pos.y >= 0 && !blizzardsPos.Contains(pos) && !walls.Contains(pos) && !newSearch.Contains(pos))
+                        if (pos.y >= 0 && !blizzardsPos.Contains(pos) && !Walls.Contains(pos) && !newSearch.Contains(pos))
                             newSearch.Enqueue(pos);
                     }
                 }
                 search = newSearch;
             } while (search.Count > 0);
             Console.WriteLine($"SEARCH 1 completed {minute}");
-            search.Enqueue(arrival);
+            search.Enqueue(Arrival);
             do
             {
                 minute++;
                 var newSearch = new Queue<(int x, int y)>();
                 // compute blizzards positions
-                var blizzardsPos = blizzardsRight.Select(e => ((e.x - 1 + minute) % width + 1, e.y)).ToHashSet();
-                blizzardsPos.UnionWith(blizzardsLeft.Select(e => (mod(e.x - 1 - minute, width) + 1, e.y)));
-                blizzardsPos.UnionWith(blizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, height) + 1)));
-                blizzardsPos.UnionWith(blizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % height + 1)));
+                var blizzardsPos = BlizzardsRight.Select(e => ((e.x - 1 + minute) % Width + 1, e.y)).ToHashSet();
+                blizzardsPos.UnionWith(BlizzardsLeft.Select(e => (mod(e.x - 1 - minute, Width) + 1, e.y)));
+                blizzardsPos.UnionWith(BlizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, Height) + 1)));
+                blizzardsPos.UnionWith(BlizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % Height + 1)));
                 while (search.TryDequeue(out var expedition))
                 {
-                    foreach (var (dx, dy) in moves)
+                    foreach (var (dx, dy) in Moves)
                     {
                         var pos = (x: expedition.x + dx, y: expedition.y + dy);
-                        if (pos == start)
+                        if (pos == Start)
                         {
                             Console.WriteLine($"FOUND {minute}");
                             newSearch.Clear();
                             search.Clear();
                             break;
                         }
-                        if (pos.y >= 0 && pos.y < input.Length && !blizzardsPos.Contains(pos) && !walls.Contains(pos) && !newSearch.Contains(pos))
+                        if (pos.y >= 0 && pos.y < Height+2 && !blizzardsPos.Contains(pos) && !Walls.Contains(pos) && !newSearch.Contains(pos))
                             newSearch.Enqueue(pos);
                     }
                 }
                 search = newSearch;
             } while (search.Count > 0);
             Console.WriteLine($"SEARCH 2 completed {minute}");
-            search.Enqueue(start);
+            search.Enqueue(Start);
             do
             {
                 minute++;
                 var newSearch = new Queue<(int x, int y)>();
                 // compute blizzards positions
-                var blizzardsPos = blizzardsRight.Select(e => ((e.x - 1 + minute) % width + 1, e.y)).ToHashSet();
-                blizzardsPos.UnionWith(blizzardsLeft.Select(e => (mod(e.x - 1 - minute, width) + 1, e.y)));
-                blizzardsPos.UnionWith(blizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, height) + 1)));
-                blizzardsPos.UnionWith(blizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % height + 1)));
+                var blizzardsPos = BlizzardsRight.Select(e => ((e.x - 1 + minute) % Width + 1, e.y)).ToHashSet();
+                blizzardsPos.UnionWith(BlizzardsLeft.Select(e => (mod(e.x - 1 - minute, Width) + 1, e.y)));
+                blizzardsPos.UnionWith(BlizzardsUp.Select(e => (e.x, mod(e.y - 1 - minute, Height) + 1)));
+                blizzardsPos.UnionWith(BlizzardsDown.Select(e => (e.x, (e.y - 1 + minute) % Height + 1)));
                 while (search.TryDequeue(out var expedition))
                 {
-                    foreach (var (dx, dy) in moves)
+                    foreach (var (dx, dy) in Moves)
                     {
                         var pos = (x: expedition.x + dx, y: expedition.y + dy);
-                        if (pos == arrival)
+                        if (pos == Arrival)
                         {
                             Console.WriteLine($"FOUND {minute}");
                             newSearch.Clear();
                             search.Clear();
                             break;
                         }
-                        if (pos.y >= 0 && !blizzardsPos.Contains(pos) && !walls.Contains(pos) && !newSearch.Contains(pos))
+                        if (pos.y >= 0 && !blizzardsPos.Contains(pos) && !Walls.Contains(pos) && !newSearch.Contains(pos))
                             newSearch.Enqueue(pos);
                     }
                 }
                 search = newSearch;
             } while (search.Count > 0);
-            return $"SEARCH 3 completed {minute}";
+            yield return $"SEARCH 3 completed {minute}";
         }
 
     }
