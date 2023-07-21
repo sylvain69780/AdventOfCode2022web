d703f788c330100c953fa45f5bc05550b10ae07d
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-07-21 23:18:03 +0200

    day1

--- AdventOfCode2022/Puzzles/CalorieCounting.cs
+++ AdventOfCode2022/Puzzles/CalorieCounting.cs
@@ -1,13 +1,21 @@
 ﻿namespace AdventOfCode2022web.Puzzles
 {
     [Puzzle(1, "Calorie Counting")]
-    public class CalorieCounting : IPuzzleSolver
+    public class CalorieCounting : IPuzzleSolverV3
     {
         private static string[] ToLines(string s) => s.Split("\n");
-        public string SolveFirstPart(string puzzleInput)
+
+        private string[]? _puzzleInput;
+
+        public void Setup(string input)
+        {
+            _puzzleInput = ToLines(input);
+        }
+
+        public IEnumerable<string> SolveFirstPart()
         {
             int sumOfCalories = 0, maxCalories = 0;
-            foreach (var value in ToLines(puzzleInput))
+            foreach (var value in _puzzleInput!)
             {
                 if (value == string.Empty)
                     sumOfCalories = 0;
@@ -15,19 +23,19 @@
                     sumOfCalories += int.Parse(value);
                 maxCalories = Math.Max(maxCalories, sumOfCalories);
             }
-             return maxCalories.ToString();
+             yield return maxCalories.ToString();
         }
-        public string SolveSecondPart(string puzzleInput)
+        public IEnumerable<string> SolveSecondPart()
         {
             var sumOfCalories = new List<int>() { 0 };
-            foreach (var value in ToLines(puzzleInput))
+            foreach (var value in _puzzleInput!)
             {
                 if (value == string.Empty)
                     sumOfCalories.Add(0);
                 else
                     sumOfCalories[^1] += int.Parse(value);
             }
-             return sumOfCalories.OrderByDescending(x => x).Take(3).Sum().ToString();
+             yield return sumOfCalories.OrderByDescending(x => x).Take(3).Sum().ToString();
         }
     }
 }
