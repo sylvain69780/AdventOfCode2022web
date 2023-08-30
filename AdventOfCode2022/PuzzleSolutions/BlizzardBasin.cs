48c25a581e30a98aa8700848734dfab0e9328975
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-08-30 15:27:26 +0200

    to be continued

--- AdventOfCode2022/PuzzleSolutions/BlizzardBasin.cs
+++ AdventOfCode2022/PuzzleSolutions/BlizzardBasin.cs
@@ -1,15 +1,55 @@
 ﻿namespace AdventOfCode2022web.Puzzles
 {
-    [Puzzle(24, "Blizzard Basin",true)]
-    public class BlizzardBasin : IPuzzleSolutionIter
+    public enum BlizzardBasinElfState
     {
-        public (int x, int y) Start;
-        public (int x, int y) Arrival;
-        public int Minute;
-        public int Width;
-        public int Height;
+        Possible,
+        Safe,
+        Killed
+    }
+    public enum Directions
+    {
+        Halt = 0,
+        Right = 1,
+        Left = 2,
+        Up = 3 ,
+        Down = 4
+    }
+
+    //BlizzardsTypes = new char[] { '>', '<', '^', 'v' }
+
+public interface IBlizzardBasinState
+    {
+        int GridWidth { get; }
+        int GridHeight { get; }
+        List<((int X, int Y) StartingPosition,(int X, int Y) TargetPosition,BlizzardBasinElfState State)>? Elves { get; }
+        List<((int X, int Y) TargetPosition, Directions)>? Blizzards { get; }
+        (int X, int Y) EntrancePosition { get; }
+        (int X, int Y) ExitPosition { get;  }
+    }
+
+    [Puzzle(24, "Blizzard Basin",true)]
+    public class BlizzardBasin : IPuzzleSolutionIter, IBlizzardBasinState
+    {
+        private int RoundNumber;
+        public (int X, int Y) EntrancePosition { get; set; }
+        public (int X, int Y) ExitPosition { get; set; }
+        public int GridWidth { get; set; }
+        public int GridHeight { get; set; }
+
+        private static readonly List<(int dx, int dy)> Moves = new()
+        {
+            (0,0),
+            (1,0),
+            (-1,0),
+            (0,-1),
+            (0,1),
+        };
+
+        public List<((int X, int Y) StartingPosition, (int X, int Y) TargetPosition, BlizzardBasinElfState State)>? Elves { get; }
+        public List<((int X, int Y) TargetPosition, Directions)>? Blizzards { get; }
+
         public Dictionary<(int x, int y, int t), (int x, int y, int t)>? Prev;
-        public List<(int x, int y)>? DeadEnds;
+        public List<(int x, int y)>? DeadEnds { get; set; }
         public bool ComputingCompleted;
 
         private List<(int x, int y, char c)>? BlizzardsRight;
@@ -27,8 +67,8 @@
 
         private void Reset()
         {
-            Start = (x: Input![0].IndexOf('.'), y: 0);
-            Arrival = (x: Input[^1].IndexOf('.'), y: Input.Length - 1);
+            EntrancePosition = ( Input![0].IndexOf('.'),  0);
+            ExitPosition = ( Input[^1].IndexOf('.'), Input.Length - 1);
             Walls = Input
                 .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
                 .Where(y => y.c == '#'))
@@ -43,10 +83,10 @@
             BlizzardsLeft = blizzards.Where(e => e.c == '<').ToList();
             BlizzardsUp = blizzards.Where(e => e.c == '^').ToList();
             BlizzardsDown = blizzards.Where(e => e.c == 'v').ToList();
-            Width = Input[0].Length - 2;
-            Height = Input.Length - 2;
+            GridWidth = Input[0].Length - 2;
+            GridHeight = Input.Length - 2;
 
-            Minute = 0;
+            RoundNumber = 0;
             ComputingCompleted = false;
             Prev = new Dictionary<(int x, int y, int t), (int x, int y, int t)>();
             DeadEnds = new List<(int x, int y)>();
@@ -54,39 +94,30 @@
 
         private static readonly char[] BlizzardsTypes = new char[] { '>', '<', '^', 'v' };
 
-        private static readonly List<(int dx, int dy)> Moves = new List<(int dx, int dy)>()
-        {
-            (0,0),
-            (1,0),
-            (-1,0),
-            (0,1),
-            (0,-1)
-        };
-
         private static int Mod(int x, int m) => (x % m + m) % m;
 
         public IEnumerable<string> SolveFirstPart()
         {
             Reset();
-            Minute = 0;
+            RoundNumber = 0;
             var newPrev = new Dictionary<(int x, int y, int t), (int x, int y, int t)>();
             ComputingCompleted = false;
             var search = new Queue<(int x, int y)>();
             Prev = new Dictionary<(int x, int y, int t), (int x, int y, int t)>();
-            search.Enqueue(Start);
+            search.Enqueue(EntrancePosition);
 
         
             bool found;
             do
             {
-                Minute++;
+                RoundNumber++;
                 var newSearch = new Queue<(int x, int y)>();
                 HashSet<(int, int y)> blizzardsPos = ComputeBlizzardsPos();
-                found = SearchForNextMove(search,newSearch, blizzardsPos,Arrival);
-                yield return $"{Minute}";
+                found = SearchForNextMove(search,newSearch, blizzardsPos,ExitPosition);
+                yield return $"{RoundNumber}";
                 search = newSearch;
             } while (search.Count > 0 && !found);
-            var p = (Arrival.x, Arrival.y, Minute);
+            var p = (x:ExitPosition.X, y:ExitPosition.Y,t: RoundNumber);
             if (!Prev.ContainsKey(p))
                 throw new InvalidDataException("No solution found");
             while (Prev.TryGetValue(p, out var np))
@@ -95,14 +126,14 @@
                 p = np;
             }
             Prev = newPrev;
-            var minute = Minute;
+            var minute = RoundNumber;
             for (var i = 1; i <= minute; i++) 
             {
-                Minute = i;
+                RoundNumber = i;
                 yield return $"Replay step {i}";
             }
             ComputingCompleted = true;
-            yield return $"{Minute}";
+            yield return $"{RoundNumber}";
         }
 
         private bool SearchForNextMove(Queue<(int x, int y)> search, Queue<(int x, int y)> newSearch, HashSet<(int, int y)> blizzardsPos,(int x,int y) arrival)
@@ -114,21 +145,21 @@
                 var branches = 0;
                 foreach (var (dx, dy) in Moves)
                 {
-                    if (head == Start && dy == -1)
+                    if (head == EntrancePosition && dy == -1)
                         continue;
-                    if (head == Arrival && dy == 1)
+                    if (head == ExitPosition && dy == 1)
                         continue;
 
                     var pos = (x: head.x + dx, y: head.y + dy);
                     if (pos == arrival)
                     {
-                        Prev!.Add((pos.x, pos.y, Minute), (head.x, head.y, Minute - 1));
+                        Prev!.Add((pos.x, pos.y, RoundNumber), (head.x, head.y, RoundNumber - 1));
                         found = true;
                         break;
                     }
                     if ( !blizzardsPos.Contains(pos) && !Walls!.Contains(pos) && !newSearch.Contains(pos))
                     {
-                        Prev!.Add((pos.x, pos.y, Minute), (head.x, head.y, Minute - 1));
+                        Prev!.Add((pos.x, pos.y, RoundNumber), (head.x, head.y, RoundNumber - 1));
                         newSearch.Enqueue(pos);
                         branches++;
                     }
@@ -146,24 +177,24 @@
         public HashSet<(int x, int y)> ComputeBlizzardsPos()
         {
             // compute blizzards positions
-            var blizzardsPos = BlizzardsRight!.Select(e => ((e.x - 1 + Minute) % Width + 1, e.y)).ToHashSet();
-            blizzardsPos.UnionWith(BlizzardsLeft!.Select(e => (Mod(e.x - 1 - Minute, Width) + 1, e.y)));
-            blizzardsPos.UnionWith(BlizzardsUp!.Select(e => (e.x, Mod(e.y - 1 - Minute, Height) + 1)));
-            blizzardsPos.UnionWith(BlizzardsDown!.Select(e => (e.x, (e.y - 1 + Minute) % Height + 1)));
+            var blizzardsPos = BlizzardsRight!.Select(e => ((e.x - 1 + RoundNumber) % GridWidth + 1, e.y)).ToHashSet();
+            blizzardsPos.UnionWith(BlizzardsLeft!.Select(e => (Mod(e.x - 1 - RoundNumber, GridWidth) + 1, e.y)));
+            blizzardsPos.UnionWith(BlizzardsUp!.Select(e => (e.x, Mod(e.y - 1 - RoundNumber, GridHeight) + 1)));
+            blizzardsPos.UnionWith(BlizzardsDown!.Select(e => (e.x, (e.y - 1 + RoundNumber) % GridHeight + 1)));
             return blizzardsPos;
         }
 
         public IEnumerable<string> SolveSecondPart()
         {
             Reset();
-            Minute = 0;
+            RoundNumber = 0;
             ComputingCompleted = false;
             var newPrev = new Dictionary<(int x, int y, int t), (int x, int y, int t)>();
             var stages = new ((int x, int y), (int x, int y))[]
             {
-                (Start,Arrival),
-                (Arrival,Start),
-                (Start,Arrival),
+                (EntrancePosition,ExitPosition),
+                (ExitPosition,EntrancePosition),
+                (EntrancePosition,ExitPosition),
             };
             foreach (var (start,arrival) in stages)
             {
@@ -173,14 +204,14 @@
                 bool found;
                 do
                 {
-                    Minute++;
+                    RoundNumber++;
                     var newSearch = new Queue<(int x, int y)>();
                     HashSet<(int, int y)> blizzardsPos = ComputeBlizzardsPos();
                     found = SearchForNextMove(search, newSearch, blizzardsPos, arrival);
-                    yield return $"{Minute}";
+                    yield return $"{RoundNumber}";
                     search = newSearch;
                 } while (search.Count > 0 && !found);
-                var p = (arrival.x,arrival.y,Minute);
+                var p = (arrival.x,arrival.y,RoundNumber);
                 if (!Prev.ContainsKey(p))
                     throw new InvalidDataException("No solution found");
                 while (Prev.TryGetValue(p, out var np))
@@ -190,14 +221,14 @@
                 }
             }
             Prev = newPrev;
-            var minute = Minute;
+            var minute = RoundNumber;
             for (var i = 1; i <= minute; i++)
             {
-                Minute = i;
+                RoundNumber = i;
                 yield return $"Replay step {i}";
             }
             ComputingCompleted = true;
-            yield return $"{Minute}";
+            yield return $"{RoundNumber}";
         }
     }
 }
