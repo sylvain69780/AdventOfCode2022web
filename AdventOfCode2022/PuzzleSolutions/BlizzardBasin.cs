a4668a5f4bc18918fcfba7f5fc08fc2be9b6ad2f
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-09-04 08:17:29 +0200

    ok

--- AdventOfCode2022/PuzzleSolutions/BlizzardBasin.cs
+++ AdventOfCode2022/PuzzleSolutions/BlizzardBasin.cs
@@ -1,6 +1,6 @@
 ﻿namespace AdventOfCode2022web.Puzzles
 {
-    public interface IBlizzardBasinViewModel
+    public interface IBlizzardBasinViewState
     {
         int GridWidth { get; }
         int GridHeight { get; }
@@ -21,23 +21,17 @@
     }
 
     [Puzzle(24, "Blizzard Basin", true)]
-    public class BlizzardBasin : IPuzzleSolutionIter, IBlizzardBasinViewModel
+    public class BlizzardBasin : IPuzzleSolutionIter, IBlizzardBasinViewState
     {
         public int GridWidth { get; set; }
         public int GridHeight { get; set; }
         public int CurrentMinute { get; set; }
         public (int X, int Y) EntrancePosition { get; set; }
         public (int X, int Y) ExitPosition { get; set; }
-        public IEnumerable<((int X, int Y) Position, Directions Direction)> BlizzardsPositions => BlizzardsPositionAtTime(CurrentMinute);
         public List<List<(int ParentId, (int X, int Y) Pos)>> Tree { get; set; } = new();
+        public IEnumerable<((int X, int Y) Position, Directions Direction)> BlizzardsPositions => GetBlizzardsPositionAtTime(CurrentMinute);
 
         private List<((int X, int Y) Position, Directions Direction)>? BlizzardsInitialPosition { get; set; }
-        private IEnumerable<((int X, int Y) Position, Directions Direction)> BlizzardsPositionAtTime(int roundNumber)
-            => BlizzardsInitialPosition!
-            .Select(b => (X: b.Position.X - 1, Y: b.Position.Y - 1, b.Direction))
-            .Select(b => (X: b.X + roundNumber * AllowedMoves[(int)b.Direction].dx, Y: b.Y + roundNumber * AllowedMoves[(int)b.Direction].dy, b.Direction))
-            .Select(b => (X: Mod(b.X, GridWidth - 2), Y: Mod(b.Y, GridHeight - 2), b.Direction))
-            .Select(b => ((b.X + 1, b.Y + 1), b.Direction));
 
         private static readonly List<(int dx, int dy)> AllowedMoves = new()
         {
@@ -47,8 +41,6 @@
             (0,-1),
             (0,1),
         };
-
-        private static int Mod(int x, int m) => (x % m + m) % m;
 
         private HashSet<(int x, int y)>? Walls;
         private string[]? Input { get; set; }
@@ -61,30 +53,69 @@
 
         private void Reset()
         {
-            GridWidth = Input![0].Length;
-            GridHeight = Input.Length;
+            ResetSimulationTime();
+            GetGridDimensions();
+            GetEntranceAndExitPositions();
+            GetWallPositions();
+            GetBlizzardsPositions();
+            InitializeBFSSearchTree();
+        }
+
+        private void ResetSimulationTime()
+        {
             CurrentMinute = 0;
-            EntrancePosition = (Input[0].IndexOf('.'), 0);
-            ExitPosition = (Input[^1].IndexOf('.'), Input.Length - 1);
-            Walls = Input
-                .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
-                .Where(y => y.c == '#'))
-                .Select(e => (x: e.col, y: e.row))
-                .ToHashSet();
-            BlizzardsInitialPosition = Input
-                .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
-                .Where(y => "><^v".Contains(y.c)))
-                .Select(e => ((x: e.col, y: e.row), e.c == '>' ? Directions.Right : e.c == '<' ? Directions.Left : e.c == '^' ? Directions.Up : Directions.Down))
-                .ToList();
+        }
+
+        private void InitializeBFSSearchTree()
+        {
             Tree.Clear();
             Tree.Add(new List<(int ParentId, (int X, int Y) Pos)>() { (0, EntrancePosition) });
         }
 
+        private void GetBlizzardsPositions()
+        {
+            BlizzardsInitialPosition = Input!
+                .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
+                .Where(y => "><^v".Contains(y.c)))
+                .Select(e => ((x: e.col, y: e.row), e.c == '>' ? Directions.Right : e.c == '<' ? Directions.Left : e.c == '^' ? Directions.Up : Directions.Down))
+                .ToList();
+        }
+
+        private void GetWallPositions()
+        {
+            Walls = Input!
+                .SelectMany((line, row) => line.Select((c, col) => (c, col, row))
+                .Where(y => y.c == '#'))
+                .Select(e => (x: e.col, y: e.row))
+                .ToHashSet();
+        }
+
+        private void GetEntranceAndExitPositions()
+        {
+            EntrancePosition = (Input![0].IndexOf('.'), 0);
+            ExitPosition = (Input[^1].IndexOf('.'), Input.Length - 1);
+        }
+
+        private void GetGridDimensions()
+        {
+            GridWidth = Input![0].Length;
+            GridHeight = Input.Length;
+        }
+
+        private static int Mod(int x, int m) => (x % m + m) % m;
+
+        private IEnumerable<((int X, int Y) Position, Directions Direction)> GetBlizzardsPositionAtTime(int roundNumber)
+            => BlizzardsInitialPosition!
+            .Select(b => (X: b.Position.X - 1, Y: b.Position.Y - 1, b.Direction))
+            .Select(b => (X: b.X + roundNumber * AllowedMoves[(int)b.Direction].dx, Y: b.Y + roundNumber * AllowedMoves[(int)b.Direction].dy, b.Direction))
+            .Select(b => (X: Mod(b.X, GridWidth - 2), Y: Mod(b.Y, GridHeight - 2), b.Direction))
+            .Select(b => ((b.X + 1, b.Y + 1), b.Direction));
+
         public IEnumerable<string> SolveFirstPart()
         {
             Reset();
-            var Levels = GetTreeLevelsAndIncrementCurrentMinute(EntrancePosition, ExitPosition);
-            foreach (var treeLevel in Levels)
+            var treeLevels = GetTreeLevelsAndIncrementCurrentMinute(EntrancePosition, ExitPosition);
+            foreach (var treeLevel in treeLevels)
             {
                 Tree.Add(treeLevel);
                 yield return $"{CurrentMinute}"; 
@@ -94,10 +125,10 @@
         public IEnumerable<string> SolveSecondPart()
         {
             Reset();
-            var levels = GetTreeLevelsAndIncrementCurrentMinute(EntrancePosition, ExitPosition)
+            var treeLevels = GetTreeLevelsAndIncrementCurrentMinute(EntrancePosition, ExitPosition)
                 .Concat(GetTreeLevelsAndIncrementCurrentMinute(ExitPosition, EntrancePosition)
                 .Concat(GetTreeLevelsAndIncrementCurrentMinute(EntrancePosition, ExitPosition)));
-            foreach (var treeLevel in levels)
+            foreach (var treeLevel in treeLevels)
             {
                 Tree.Add(treeLevel);
                 yield return $"{CurrentMinute}";
@@ -114,7 +145,7 @@
             do
             {
                 CurrentMinute++;
-                var blizzardsPosition = BlizzardsPositionAtTime(CurrentMinute).Select(b => (b.Position.X, b.Position.Y)).ToHashSet();
+                var blizzardsPosition = GetBlizzardsPositionAtTime(CurrentMinute).Select(b => (b.Position.X, b.Position.Y)).ToHashSet();
                 var newQueue = new List<(int ParentId, (int X, int Y) Pos)>();
                 for (var parentId = 0; parentId < queue.Count && !found; parentId++)
                 {
