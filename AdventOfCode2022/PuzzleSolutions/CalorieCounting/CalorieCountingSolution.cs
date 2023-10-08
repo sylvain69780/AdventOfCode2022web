18c16cd699ed5b7e2157a9b1b8cb7e957009953c
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-10-08 20:05:28 +0200

    Calories

--- AdventOfCode2022/PuzzleSolutions/CalorieCounting/CalorieCountingSolution.cs
+++ AdventOfCode2022/PuzzleSolutions/CalorieCounting/CalorieCountingSolution.cs
@@ -1,12 +1,15 @@
-﻿using AdventOfCode2022Solutions.PuzzleSolutions;
-
-namespace AdventOfCode2022Solutions.PuzzleSolutions.CalorieCounting
+﻿namespace AdventOfCode2022Solutions.PuzzleSolutions.CalorieCounting
 {
     public class CalorieCountingSolution : IPuzzleSolutionIter
     {
         private static string[] ToLines(string s) => s.Split("\n");
 
         private string[]? _puzzleInput;
+
+        public string[]? GroupsOfElves => _puzzleInput;
+
+        private int sumOfCalories;
+        public int CurrentSum => sumOfCalories;
 
         public void Initialize(string input)
         {
@@ -15,7 +18,8 @@ namespace AdventOfCode2022Solutions.PuzzleSolutions.CalorieCounting
 
         public IEnumerable<string> SolveFirstPart()
         {
-            int sumOfCalories = 0, maxCalories = 0;
+            sumOfCalories = 0;
+                int maxCalories = 0;
             foreach (var value in _puzzleInput!)
             {
                 if (value == string.Empty)
@@ -24,24 +28,26 @@ namespace AdventOfCode2022Solutions.PuzzleSolutions.CalorieCounting
                 {
                     sumOfCalories += int.Parse(value);
                     maxCalories = Math.Max(maxCalories, sumOfCalories);
-                    yield return $"The current group of Elves carries {sumOfCalories} calories.\nCurrent max value is {maxCalories}";
                 }
+                yield return $"The current group of Elves carries {sumOfCalories} calories.\nCurrent max value is {maxCalories}";
             }
             yield return maxCalories.ToString();
         }
         public IEnumerable<string> SolveSecondPart()
         {
-            var sumOfCalories = new List<int>() { 0 };
+            sumOfCalories = 0;
+            var sumsOfCalories = new List<int>() { 0 };
             foreach (var value in _puzzleInput!)
             {
                 if (value == string.Empty)
-                    sumOfCalories.Add(0);
+                    sumsOfCalories.Add(0);
                 else
-                    sumOfCalories[^1] += int.Parse(value);
+                    sumsOfCalories[^1] += int.Parse(value);
+                sumOfCalories = sumsOfCalories[^1]; // for the visualization
                 yield return "Top 3 of Elves groups holding the more calories:\n"
-                    + string.Join('\n', Top3(sumOfCalories).Select(x => x.ToString()));
+                    + string.Join('\n', Top3(sumsOfCalories).Select(x => x.ToString()));
             }
-            yield return Top3(sumOfCalories).Sum().ToString();
+            yield return Top3(sumsOfCalories).Sum().ToString();
 
             static IEnumerable<int> Top3(List<int> sumOfCalories) => sumOfCalories.OrderByDescending(x => x).Take(3);
         }
