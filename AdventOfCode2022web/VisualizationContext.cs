5173fdf296427e6b51bcdef949eb52196c2db789
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2023-11-11 21:48:36 +0100

    woring

---
+++ AdventOfCode2022web/VisualizationContext.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2022web
+{
+    public class VisualizationContext<TData>
+    {
+        public TData? Data { get; set; }
+        public VisualizationSettings? Settings { get; set; }
+    }
+}
